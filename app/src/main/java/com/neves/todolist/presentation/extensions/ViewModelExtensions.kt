71231874ebccda31521406513a5ff27cc3656939
Augusto Flávio <augusto.flavio@work.co>
2022-01-15 15:48:35 -0300

    fix: remove unnecessary extension

--- app/src/main/java/com/neves/todolist/presentation/extensions/ViewModelExtensions.kt
+++ app/src/main/java/com/neves/todolist/presentation/extensions/ViewModelExtensions.kt
@@ -8,23 +8,6 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
-fun <T> ViewModel.launchOnUiState(uiState: MutableStateFlow<UiState<T>>, call: suspend () -> T, update: (UiState<T>, T) -> T) {
-    uiState.update { UiState.Loading }
-    viewModelScope.launch {
-        try {
-            val result = call.invoke()
-
-            uiState.update { current ->
-                UiState.Success(
-                    update.invoke(current, result)
-                )
-            }
-        } catch (e: Exception) {
-            UiState.Failure(e)
-        }
-    }
-}
-
 fun <T> ViewModel.launchOnUiState(uiState: MutableStateFlow<UiState<T>>, call: suspend () -> T) {
     uiState.update { UiState.Loading }
     viewModelScope.launch {
